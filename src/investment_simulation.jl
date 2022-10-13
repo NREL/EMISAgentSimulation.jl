94e82c320331481ca779db95e206364fb36a8c27
yinongsun <yinong.sun92@gmail.com>
2022-10-13 10:47:41 -0600

    Fix bug on missing PSY. and PSYE.

--- src/investment_simulation.jl
+++ src/investment_simulation.jl
@@ -125,10 +125,10 @@ function run_agent_simulation(simulation::AgentSimulation, simulation_years::Int
         #Find which markets to simulate.
         markets = union(hcat(get_markets.(get_investors(simulation))...))
 
-        for d in get_components(ThermalCleanEnergy,sys_UC)
+        for d in PSY.get_components(PSYE.ThermalCleanEnergy,sys_UC)
             if "CleanEnergyConstraint" ∉ PSY.get_name.(d.services)
                 println("$(PSY.get_name(d))")
-                EAS.add_clean_energy_contribution!(sys_UC, d)
+                add_clean_energy_contribution!(sys_UC, d)
             end
         end
 
